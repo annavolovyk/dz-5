62ac044911108649a20f032c46c37dd04b29c471
Anna Volovyk <volovyk.anna@gmail.com>
2023-04-01 12:51:54 +0300

    dz-5 (updated)

--- Main.java
+++ Main.java
@@ -1,19 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int a = 10;
-        int b = 20;
+        int b = 100;
         int c = 30;
-        int d = 40;
+        int d = -10;
 
         int sum1 = a+b;
         int sum2 = c+d;
-        System.out.println(sum1 < sum2);
+        System.out.println(sum1 > sum2);
 
         sum1 = sum1++;
-        sum2 = sum2--;
-        sum2 = sum2--;
+        sum2 -= 2;
 
-        System.out.println(sum1 < sum2);
+        System.out.println(sum1 > sum2);
 
         System.out.println(sum1 % 2 == 0 || sum2 % 2 == 0);
 
